edb3cb3d50d47a7fba36ef51bfdfa522a7a017cc
oz izhak <oz@web-pick.com>
2019-07-18 19:28:44 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+// ANGULAR BUILD IN MODULES
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// COSTUM MODULES
+import {SharedModule} from './shared/shared.module';
+
+
+//COMPONENTS
+import { AppComponent } from './app.component';
+import { DriversComponent } from './drivers/drivers.component';
+import { MapComponent } from './map/map.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DriversComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
